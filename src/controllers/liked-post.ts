080db23c2bbc62ecd6037669405ae2fe6f34f196
Martin Rada <martinradadl@gmail.com>
2025-06-19 10:24:11 -0500

    liked posts endpoints added

--- src/controllers/liked-post.ts
+++ src/controllers/liked-post.ts
@@ -7,24 +7,25 @@ export const getUsersByPostId = async (req: Request, res: Response) => {
 
     const likes = await likedPostModel.LikedPost.find({ postId });
     const users = likes.map((like) => like.userId);
-    return res.status(200).json(users);
+    res.status(200).json(users);
   } catch (err: unknown) {
     if (err instanceof Error) {
-      return res.status(500).json({ message: err.message });
+      res.status(500).json({ message: err.message });
     }
   }
 };
 
 export const add = async (req: Request, res: Response) => {
   try {
+    const { userId, postId } = req.body;
     const newLikedPost = await likedPostModel.LikedPost.create({
-      userId: req.body.userId,
-      postId: req.body.postId,
+      userId,
+      postId,
     });
-    return res.status(200).json(newLikedPost);
+    res.status(200).json(newLikedPost);
   } catch (err: unknown) {
     if (err instanceof Error) {
-      return res.status(500).json({ message: err.message });
+      res.status(500).json({ message: err.message });
     }
   }
 };
@@ -35,15 +36,15 @@ export const deleteOne = async (req: Request, res: Response) => {
       req.params.id
     );
     if (!deletedLikedPost) {
-      return res.status(404).json({
+      res.status(404).json({
         message: "Delete not successful",
         error: "Liked Post not found",
       });
     }
-    return res.status(200).json(deletedLikedPost);
+    res.status(200).json(deletedLikedPost);
   } catch (err: unknown) {
     if (err instanceof Error) {
-      return res.status(500).json({ message: err.message });
+      res.status(500).json({ message: err.message });
     }
   }
 };
