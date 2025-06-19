1dcbbdcb0796c9dd204f9a23ac2fc8bd6906eeb4
Martin Rada <martinradadl@gmail.com>
2025-06-19 10:25:36 -0500

    shared posts endpoints added

--- src/controllers/shared-post.ts
+++ src/controllers/shared-post.ts
@@ -7,10 +7,10 @@ export const getUsersByPostId = async (req: Request, res: Response) => {
 
     const shares = await sharedPostModel.SharedPost.find({ postId });
     const users = shares.map((share) => share.userId);
-    return res.status(200).json(users);
+    res.status(200).json(users);
   } catch (err: unknown) {
     if (err instanceof Error) {
-      return res.status(500).json({ message: err.message });
+      res.status(500).json({ message: err.message });
     }
   }
 };
@@ -21,24 +21,25 @@ export const getPostsByUserId = async (req: Request, res: Response) => {
 
     const shares = await sharedPostModel.SharedPost.find({ userId });
     const posts = shares.map((share) => share.postId);
-    return res.status(200).json(posts);
+    res.status(200).json(posts);
   } catch (err: unknown) {
     if (err instanceof Error) {
-      return res.status(500).json({ message: err.message });
+      res.status(500).json({ message: err.message });
     }
   }
 };
 
 export const add = async (req: Request, res: Response) => {
   try {
+    const { userId, postId } = req.body;
     const newSharedPost = await sharedPostModel.SharedPost.create({
-      userId: req.body.userId,
-      postId: req.body.postId,
+      userId,
+      postId,
     });
-    return res.status(200).json(newSharedPost);
+    res.status(200).json(newSharedPost);
   } catch (err: unknown) {
     if (err instanceof Error) {
-      return res.status(500).json({ message: err.message });
+      res.status(500).json({ message: err.message });
     }
   }
 };
@@ -48,15 +49,15 @@ export const deleteOne = async (req: Request, res: Response) => {
     const deletedSharedPost =
       await sharedPostModel.SharedPost.findByIdAndDelete(req.params.id);
     if (!deletedSharedPost) {
-      return res.status(404).json({
+      res.status(404).json({
         message: "Delete not successful",
         error: "SharedPost not found",
       });
     }
-    return res.status(200).json(deletedSharedPost);
+    res.status(200).json(deletedSharedPost);
   } catch (err: unknown) {
     if (err instanceof Error) {
-      return res.status(500).json({ message: err.message });
+      res.status(500).json({ message: err.message });
     }
   }
 };
