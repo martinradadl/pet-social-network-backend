b91a3ef4bc110876451a325e302e194ae624bbda
Martin Rada <martinradadl@gmail.com>
2025-06-19 10:15:12 -0500

    routes added to project index

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,17 @@ import http from "http";
 import cors from "cors";
 import { APP_URL } from "./helpers/global";
 import cookieParser from "cookie-parser";
+import auth from "./routes/auth";
+import chats from "./routes/chat";
+import comments from "./routes/comment";
+import directMessages from "./routes/direct-message";
+import follows from "./routes/follow";
+import likedComments from "./routes/liked-comment";
+import likedPosts from "./routes/liked-post";
+import posts from "./routes/post";
+import savedPosts from "./routes/saved-post";
+import sharedPosts from "./routes/shared-post";
+import stories from "./routes/story";
 
 const jsonParser = bodyParser.json();
 
@@ -21,6 +32,18 @@ app.use(express.static("public"));
 // @ts-expect-error error pending
 app.get("/", (_, res) => res.send("Server is running"));
 app.use("/uploads", express.static("uploads"));
+
+app.use("/auth", auth);
+app.use("/chats", chats);
+app.use("/comments", comments);
+app.use("/directMessages", directMessages);
+app.use("/follows", follows);
+app.use("/likedComments", likedComments);
+app.use("/likedPosts", likedPosts);
+app.use("/posts", posts);
+app.use("/savedPosts", savedPosts);
+app.use("/sharedPosts", sharedPosts);
+app.use("/stories", stories);
 
 // Start the server and listen on the specified port
 httpServer.listen(port, () => {
