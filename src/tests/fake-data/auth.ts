522353273e038a642f4acf6dfe9923c39e9d3a9c
Martin Rada <martinradadl@gmail.com>
2025-07-16 12:29:14 -0500

    Auth tests created

---
+++ src/tests/fake-data/auth.ts
@@ -0,0 +1,23 @@
+export const fakeToken = "fakeToken";
+
+export const fakeUser = {
+  _id: "fakeId",
+  email: "fakeEmail",
+  username: "fakeUsername",
+  password: "fakePassword",
+  name: "fakeName",
+  bio: "fakeBio",
+  profilePic: "fakeProfilePic",
+  isPrivate: false,
+};
+
+export const fakeUser2 = {
+  _id: "fakeId2",
+  email: "fakeEmail2",
+  username: "fakeUsername2",
+  password: "fakePassword2",
+  name: "fakeName2",
+  bio: "fakeBio2",
+  profilePic: "fakeProfilePic2",
+  isPrivate: true,
+};
